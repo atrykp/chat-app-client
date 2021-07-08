42b419ac8d756fc934a8c5762bd1d0962eb51cae
Patryk Plaza <atrykp0@gmail.com>
2021-07-08 17:31:07 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
+import axios from "axios";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const socket = io("http://localhost:5000");
+  const getdata = async () => {
+    const { data } = await axios.get("http://localhost:5000/");
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, welcome in chat app</h1>
     </div>
   );
 }
