6421b2f8b020f4b94127c012d0d38b9b8ce5f64c
Patryk Plaza <atrykp0@gmail.com>
2021-07-15 06:15:25 +0200

    add styles to chat screen

--- src/Screen/ChatScreen/ChatScreen.tsx
+++ src/Screen/ChatScreen/ChatScreen.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
 import Button from "../../components/Button";
 import "./ChatScreen.scss";
 
 const ChatScreen = () => {
+  const history = useHistory();
   return (
     <div className="chat-screen-wrapper">
-      <div className="chat-screen-bar"></div>
+      <div className="chat-screen-bar">
+        <button className="chat-bar-button" onClick={() => history.goBack()}>
+          {"<"}
+        </button>
+        <p className="chat-bar-header">patryko</p>
+      </div>
       <div className="chat-screen-conversation"></div>
-      <div className="chat-screen-form">
-        <input className="chat-screen-form-input"></input>
-        <Button styles="send-button">Send</Button>
+      <div className="chat-screen-form-wrapper">
+        <form action="" className="chat-screen-form">
+          <input className="chat-screen-form-input"></input>
+          <Button styles="send-button">Send</Button>
+        </form>
       </div>
     </div>
   );
