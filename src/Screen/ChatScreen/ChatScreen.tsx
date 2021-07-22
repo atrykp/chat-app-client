da6465366eae079284dbe7fe3d67bba9a10a8e96
Patryk Plaza <atrykp0@gmail.com>
2021-07-22 05:48:20 +0200

    add form to chat screen

--- src/Screen/ChatScreen/ChatScreen.tsx
+++ src/Screen/ChatScreen/ChatScreen.tsx
@@ -1,5 +1,6 @@
 import { useHistory, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
+import { useForm, SubmitHandler } from "react-hook-form";
 import Button from "../../components/Button";
 import Message from "../../components/Message/Message";
 import "./ChatScreen.scss";
@@ -7,7 +8,17 @@ import { useAppSelector } from "../../hooks/redux-hooks";
 import axios from "axios";
 import { useState } from "react";
 
+type Inputs = {
+  textInput: string;
+};
+
 const ChatScreen = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Inputs>();
   const userInfo = useAppSelector((state) => state.user);
   const [messagesList, setMessagesList] = useState([]);
   const { id: conversationId } = useParams<{ id: string }>();
@@ -49,6 +60,10 @@ const ChatScreen = () => {
     });
     setMessagesList(messagesList);
   };
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <div className="chat-screen-wrapper">
@@ -63,8 +78,15 @@ const ChatScreen = () => {
       </div>
       <div className="chat-screen-conversation">{messagesList}</div>
       <div className="chat-screen-form-wrapper">
-        <form action="" className="chat-screen-form">
-          <input className="chat-screen-form-input"></input>
+        <form
+          action=""
+          className="chat-screen-form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            className="chat-screen-form-input"
+            {...register("textInput", { required: true })}
+          ></input>
           <Button styles="send-button">Send</Button>
         </form>
       </div>
