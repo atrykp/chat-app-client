ff0a073db4fd8617522bc33c19a9465e44bba3da
Patryk Plaza <atrykp0@gmail.com>
2021-08-01 07:07:00 +0200

    add confirm modal during remove user in UserScreen

--- src/Screen/UserScreen/UserScreen.tsx
+++ src/Screen/UserScreen/UserScreen.tsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import { useHistory } from "react-router";
+
 import Button from "../../components/Button";
+import ConfirmModal from "../../components/ConfirmModal/ConfirmModal";
+
 import { useAppSelector } from "../../hooks/redux-hooks";
+import { useAxios } from "../../hooks/useAxios";
 import { useLogout } from "../../hooks/useLogout";
+
 import "./UserScreen.scss";
 
 const UserScreen = () => {
+  const [isConfirm, setIsConfirm] = useState(false);
   const userInfo = useAppSelector((state) => state.user);
   const history = useHistory();
   const logout = useLogout();
+  const { authAxiosDelete } = useAxios(userInfo.token);
+
+  const removeAccount = async () => {
+    await authAxiosDelete("http://localhost:5000/user");
+    logout();
+  };
   return (
     <div className="user-screen-wrapper">
+      {isConfirm && (
+        <ConfirmModal
+          text="Are you sure?"
+          confirmCallback={removeAccount}
+          closeCallback={() => setIsConfirm(false)}
+        />
+      )}
+
       <div className="user-screen-header">
         <p>hello {userInfo.username}!</p>
         <button
@@ -33,7 +54,9 @@ const UserScreen = () => {
       </div>
       <div className="user-screen-buttons">
         <Button callback={() => logout()}>Logout</Button>
-        <Button styles="user-screen-remove">Remove Account</Button>
+        <Button styles="user-screen-remove" callback={() => setIsConfirm(true)}>
+          Remove Account
+        </Button>
       </div>
     </div>
   );
