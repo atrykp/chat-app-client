1eb09f026a4bbc892fd962828bac67e2a091053f
Patryk Plaza <atrykp0@gmail.com>
2021-08-14 10:51:15 +0200

    fix problem with infinite useEffect in chat Screen

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,19 +1,23 @@
 import axios from "axios";
+import { useCallback, useRef } from "react";
 
 export const useAxios = (token: string) => {
-  const config = {
+  const configRef = useRef({
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  };
-  const authAxiosGet = async (url: string) => await axios.get(url, config);
+  });
+  const authAxiosGet = useCallback(
+    async (url: string) => await axios.get(url, configRef.current),
+    []
+  );
   const authAxiosPost = async (url: string, data: any) =>
-    await axios.post(url, data, config);
+    await axios.post(url, data, configRef.current);
   const authAxiosPut = async (url: string, data: any) =>
-    await axios.put(url, data, config);
+    await axios.put(url, data, configRef.current);
   const authAxiosDelete = async (url: string) =>
-    await axios.delete(url, config);
+    await axios.delete(url, configRef.current);
 
   return { authAxiosGet, authAxiosPost, authAxiosPut, authAxiosDelete };
 };
